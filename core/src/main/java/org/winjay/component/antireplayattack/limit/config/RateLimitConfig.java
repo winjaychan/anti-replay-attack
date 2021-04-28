4da8651214e5a0d688915c1673f21ceb46d46bbf
winjay <winjay_chan@qq.com>
2021-04-28 11:41:10 +0800

    feat(config): config支持JSON字符串数组
    （1）限流配置支持JSON字符串数组

--- core/src/main/java/org/winjay/component/antireplayattack/limit/config/RateLimitConfig.java
+++ core/src/main/java/org/winjay/component/antireplayattack/limit/config/RateLimitConfig.java
@@ -1,7 +1,12 @@
 package org.winjay.component.antireplayattack.limit.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.winjay.component.antireplayattack.limit.model.RateLimitInterfaceDef;
 
 import java.util.List;
@@ -13,13 +18,18 @@ import java.util.List;
  * @date 2021/3/5 14:29
  */
 @ConfigurationProperties(prefix = "config.rate-limit")
+@Slf4j
 public class RateLimitConfig {
     private List<RateLimitInterfaceDef> rateLimitInterfaces;
     private String prefix;
     private String type;
+    private String rateLimitInterfaceString;
+    private boolean arrayConfigEnabled = true;
 
     public void setRateLimitInterfaces(List<RateLimitInterfaceDef> rateLimitInterfaces) {
-        this.rateLimitInterfaces = rateLimitInterfaces;
+        if (arrayConfigEnabled){
+            this.rateLimitInterfaces = rateLimitInterfaces;
+        }
     }
 
     public List<RateLimitInterfaceDef> getRateLimitInterfaces() {
@@ -40,5 +50,42 @@ public class RateLimitConfig {
 
     public String getType() {
         return type;
+    }
+
+    public String getRateLimitInterfaceString() {
+        return rateLimitInterfaceString;
+    }
+
+    public void setRateLimitInterfaceString(String rateLimitInterfaceString) {
+        this.rateLimitInterfaceString = rateLimitInterfaceString;
+        if (arrayConfigEnabled){
+            return;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        if (ObjectUtils.isEmpty(rateLimitInterfaceString)){
+            return ;
+        }
+        try {
+            rateLimitInterfaces = mapper.readValue(rateLimitInterfaceString,  new TypeReference<List<RateLimitInterfaceDef>>() {
+            });
+        } catch (JsonProcessingException e) {
+            log.warn("限流配置解析异常，rateLimitInterfaceString={}", rateLimitInterfaceString);
+        }
+    }
+
+    public static void main(String[] args) throws JsonProcessingException {
+        String json = "[{\"method\": \"put\", \"uri\": \"/relayattack/{tenantid}/role/{roleid}\", \"limit\": 6, \"limitPeriod\": 60},{\"method\": \"put\", \"uri\": \"/relayattackxx/{tenantid}/role/{roleid}\", \"limit\": 6, \"limitPeriod\": 60}]";
+        ObjectMapper mapper = new ObjectMapper();
+        List<RateLimitInterfaceDef> rateLimitInterfaceDefs = mapper.readValue(json, new TypeReference<List<RateLimitInterfaceDef>>() {
+        });
+        System.out.println(rateLimitInterfaceDefs);
+    }
+
+    public boolean isArrayConfigEnabled() {
+        return arrayConfigEnabled;
+    }
+
+    public void setArrayConfigEnabled(boolean arrayConfigEnabled) {
+        this.arrayConfigEnabled = arrayConfigEnabled;
     }
 }
